bf031bc25dc21b63f2723831f8c106ae41322343
Ayala Weinstock <apap90948@gmail.com>
2024-02-05 00:41:09 +0200

    App component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailsComponent },
+  { path: 'user/:id/details', component: UserDetailsComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
